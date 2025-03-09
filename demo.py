60854176c2d5e70aeaf10de912b6ab46a2a0353e
Christine Arroyo <chrstinearroyo@gmail.com>
2025-03-09 16:05:56 +0800

    Create demo.py

---
+++ demo.py
@@ -0,0 +1,25 @@
+import sys
+print(sys.version)
+
+msg = "Boss Tingen Code"
+print(msg)
+
+x = "Lipat na sa Python?"
+y = " Oo aya lintek"
+print (x + y)
+
+#weird namang mag comment hashtag
+
+var_name = 'itigil na yan'
+print(var_name)
+
+# if-else statement
+a = 10
+b = 25
+if a < b:
+    print('mababa ang letrang A')
+
+# For Loop
+students = [1,2,3,4,5,6,7,8,9,10]
+for i in students:
+    print(i)
